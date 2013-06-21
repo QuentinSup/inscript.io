9c575c437adb33626610d96e8eaff24ef8cddb8e
QuentinSup <qsupernant@gmail.com>
2013-06-21 18:31:58 +0200

    Update subscribe parameters

--- subscribe.php
+++ subscribe.php
@@ -7,14 +7,14 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if($method == 'GET') {
 	$feedId  = $_GET['feedId'];
-	$results = DB::query('SELECT * FROM subscribes WHERE sub_fed_id=%s', $feedId);
+	$results = DB::query('SELECT accounts.* FROM subscribes, accounts WHERE sub_acc_id = acc_id AND sub_fed_id=%s', $feedId);
 	echo json_encode(array('message' => 'OK', 'data' => $results));
 } elseif($method == 'POST') {
 	$feedId = $_GET['feedId'];
 	$accountId = $_GET['accountId'];
 	$data = array('sub_fed_id' => $feedId, 'sub_acc_id' => $accountId);
 	try {
-		DB::insert('subsribes', $data);
+		DB::insert('subscribes', $data);
 		$data['sub_id'] = DB::insertId();
 		echo json_encode(array('message' => 'OK', 'data' => $data));
 	} catch(exception $e) {
